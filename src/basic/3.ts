2ad198315c86173a52475881d9f110db97d02755
Kateryna <katerynawalls@proton.me>
2024-12-21 17:17:35 -0600

    basic

---
+++ src/basic/3.ts
@@ -0,0 +1,10 @@
+let mixedType: string | number;
+mixedType = 'string';
+mixedType = 18;
+
+type enableDisable = 'enable' | 'disable';
+let able: enableDisable;
+able = 'enable';
+able = 'disable';
+
+export{};
